0c595a79328e2b4bfc5437ebb4bb0b79153e6856
zl <zl@zlzyz.com>
2017-07-18 12:15:02 +0100

    update

--- py/gff2fasta.py
+++ py/gff2fasta.py
@@ -3,8 +3,8 @@
 """
 Get dna sequences based on a gff file for features like gene, transcript, cds, exon, and get pep for coding sequences.
 SHOULD CHECK THE FEATURE NAME IN YOUR GFF: mRNA or transcript, and change accordingly.
-Usage: python gff2fasta.py <gff3 file> <genome sequence> <feature> > output
-last update: 17/07/2017
+Usage: python gff2fasta.py <gff3 file> <genome sequence> <type> > output
+last update: 18/07/2017
 """
 
 import sys
@@ -17,23 +17,36 @@ import warnings
 from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonWarning)
 
-myGFF = sys.argv[1]
-myFasta = sys.argv[2]
+if len(sys.argv) < 3:
+    sys.exit("Usage: python gff2fasta.py <gff3 file> <genome sequence> <type> > output")
+else:
+    myGFF = sys.argv[1]
+    myFasta = sys.argv[2]
+    seqType = sys.argv[3]
 
 db = gffutils.create_db(myGFF, ':memory:', merge_strategy="create_unique", keep_order=True)
+
+## or create a real database
+## import data into database: one-time operation
+#db = gffutils.create_db(myGFF, 'myGFF.db', merge_strategy="create_unique", keep_order=True) # ':memory:'
+## once created, use the database (donot need gff file anymore)
+#db = gffutils.FeatureDB('myGFF.db')
 
 ## function to get all gene/transcript(mRNA) sequences
 def parent_seq(type):
     for p in db.features_of_type(type):
+        print('>' + p.id)
         p_seq = p.sequence(myFasta)
         p_seq = Seq(p_seq, generic_dna)
         if p.strand == '-':
             p_seq = p_seq.reverse_complement()
-        print('>' + p.id + '\n' + p_seq)
+        for i in range(0, len(p_seq), 60): # print 60 bases per line
+            print(p_seq[i:i+60])
 
 ## function to get all cds/exon/intron sequences under the same transcript id
 def child_seq(type):
     for t in db.features_of_type('transcript', order_by='start'): # or mRNA depending on the gff
+        print('>' + t.id + '_' + type)
         # print(t.sequence(myFasta))
         seq_combined = ''
         for i in db.children(t, featuretype=type, order_by='start'): # or exon/intron
@@ -42,12 +55,14 @@ def child_seq(type):
         seq_combined = Seq(seq_combined, generic_dna)
         if t.strand == '-':
             seq_combined = seq_combined.reverse_complement()
-        print('>' + t.id + '\n' + seq_combined)
+        for i in range(0, len(seq_combined), 60):
+            print(seq_combined[i:i+60])
 
 ## function to get all protein sequences under the same transcript id
 def pep_seq():
     for t in db.features_of_type('transcript', order_by='start'): # or mRNA depending on the gff
         # print(t.sequence(myFasta))
+        print('>' + t.id)
         seq_combined = ''
         for i in db.children(t, featuretype='CDS', order_by='start'): # or exon/intron
             seq = i.sequence(myFasta, use_strand=True)  # use_strand doesn't work; have to revcomp
@@ -55,9 +70,9 @@ def pep_seq():
         seq_combined = Seq(seq_combined, generic_dna)
         if t.strand == '-':
             seq_combined = seq_combined.reverse_complement()
-        print('>' + t.id + '\n' + seq_combined.translate())
-
-seqType = sys.argv[3]
+        seq_transl = seq_combined.translate()
+        for i in range(0, len(seq_transl), 60):
+            print(seq_transl[i:i+60])
 
 if seqType in ['gene', 'transcript']:
     parent_seq(seqType)
