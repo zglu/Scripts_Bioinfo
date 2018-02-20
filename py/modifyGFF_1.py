0b5611b827b6635f7d014af9524faac49d735e26
zl <zl@zlzyz.com>
2018-02-20 22:26:08 +0000

    update

---
+++ py/modifyGFF_1.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+'''
+modify GFF from Apollo to Augustus style
+this script will rename transcript as .1, .2, .3, regardless of the naming in gff; duplicate gene features are not allowed
+must be in gene -> mRNA -> cds/exon order
+first remove comment and empty lines: grep -v '#' | grep -v -e '^$'
+'''
+
+# last update: 21/12/2017
+
+import re
+import sys
+
+GFF = sys.argv[1] # gff with many genes
+
+
+with open(GFF) as fin:
+    for line in fin:
+        line = line.rstrip()
+        line = line.split()
+        try:
+            if any(s in line for s in ("gene", "pseudogene")):
+                i = 0
+                name_pat = re.compile("Name=(Smp_......);")
+                geneid = name_pat.search(line[8]).group(1)
+                line[8] = "ID=" + geneid
+            elif any(s in line for s in ("mRNA", "transcript")):
+                mrnaid_pat = re.compile("ID=(.+?);")
+                mrnaid = mrnaid_pat.search(line[8]).group(1)
+                i += 1
+                trid = geneid + "." + str(i)
+                line[8] = "ID=" + trid + ";" + "Parent=" + geneid
+            elif any(s in line for s in ("exon", "non_canonical_three_prime_splice_site", "non_canonical_five_prime_splice_site")):
+                line[8] = line[8].replace(mrnaid, trid)
+                line[8] = re.sub(";ID=(.+)", "", line[8])
+                line[8] = re.sub("Name=(.+)", "", line[8])
+            elif "CDS" in line:
+                line[8] = line[8].replace(mrnaid, trid)
+                line[8] = re.sub("Parent=(.+?);", "", line[8])
+                line[8] = re.sub("ID=(.+);", "ID=" + trid + ".cds;Parent=" + trid, line[8])
+                line[8] = re.sub("Name=(.+)", "", line[8])
+            line[1] = "Apollo"
+            print("\t".join(line))
+        except (AttributeError, NameError):
+            pass
---
+++ py/modifyGFF_2.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+'''
+modify GFF from Apollo to Augustus style
+this will take transcript name as it is; can have multiple gene features
+must be in gene -> mRNA -> cds/exon order
+first remove comment and empty lines: grep -v '#' | grep -v -e '^$'
+'''
+
+# last update: 21/12/2017
+
+import re
+import sys
+
+GFF = sys.argv[1] # gff with many genes
+
+
+with open(GFF) as fin:
+    for line in fin:
+        line = line.rstrip()
+        line = line.split()
+        try:
+            if any(s in line for s in ("gene", "pseudogene")):
+                name_pat = re.compile("Name=(Smp_......);")
+                geneid = name_pat.search(line[8]).group(1)
+                line[8] = "ID=" + geneid
+            elif any(s in line for s in ("mRNA", "transcript")):
+                mrnaid_pat = re.compile("ID=(.+?);")
+                mrnaid = mrnaid_pat.search(line[8]).group(1)
+                trid_pat = re.compile("Name=(Smp_......\.(.)+?);")
+                trid = trid_pat.search(line[8]).group(1)
+                line[8] = "ID=" + trid + ";" + "Parent=" + geneid
+            elif any(s in line for s in ("exon", "non_canonical_three_prime_splice_site", "non_canonical_five_prime_splice_site")):
+                line[8] = line[8].replace(mrnaid, trid)
+                line[8] = re.sub(";ID=(.+)", "", line[8])
+                line[8] = re.sub("Name=(.+)", "", line[8])
+            elif "CDS" in line:
+                line[8] = line[8].replace(mrnaid, trid)
+                line[8] = re.sub("Parent=(.+?);", "", line[8])
+                line[8] = re.sub("ID=(.+);", "ID=" + trid + ".cds;Parent=" + trid, line[8])
+                line[8] = re.sub("Name=(.+)", "", line[8])
+            line[1] = "Apollo"
+            print("\t".join(line))
+        except (AttributeError, NameError):
+            pass
