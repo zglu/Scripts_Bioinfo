8787ea58e6e2918b7bda4365209b1b861e2a6c27
zglu <luzg2008@gmail.com>
2019-06-26 10:19:18 +0100

    minor update

--- sh/getSeq_aroundTSS.sh
+++ sh/getSeq_aroundTSS.sh
@@ -5,3 +5,5 @@
 
 grep -f $1 ~/Documents/SCHISTO/V7/annotations/Smv7.3/Sm_v7.3_Basic-noGaps.gff > $1.gff
 python3 ~/Google\ Drive/Scripts_Bioinfo/py/up-downstream_sequence.py $1.gff ~/Documents/SCHISTO/V7/Smansoni_v7.fa gene $2 $3 > ${1}_up$2-down$3.fa
+
+rm -f $1.gff
