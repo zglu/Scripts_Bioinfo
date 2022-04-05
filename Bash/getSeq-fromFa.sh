0da5e94fee72546b48e8c89e1c4728b93771b88b
Z. Lu <luzg2008@gmail.com>
2022-04-05 12:39:16 +0200

    update script

--- Bash/getSeq-fromFa.sh
+++ Bash/getSeq-fromFa.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-    for id in $(cut -f1 s7newids); do
-    (cat WBPS7.Sm.protein.fa |sed /$id/',/>/!d'| awk '/>/{i++}i==1' > ./$id.fa)
-    done
+
+# usage: ./getSeq-fromFa.sh [id] [fasta file]
+
+sed /$1/',/>/!d' $2 | awk '/>/{i++}i==1'
+
+#eg: sed /Smp_076300/',/>/!d' ~/zl3/SCHISTO/v9/Sm_v9.pep.fa | awk '/>/{i++}i==1'
+
+# batch
+#for id in $(cut -f1 IDLIST); do
+#(sed /$id/',/>/!d' FASTAFILE| awk '/>/{i++}i==1' > ./$id.fa)
+#done
+
